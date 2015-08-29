562c399ea8a5ccde3304ed7d3bc206bba2ad0c12
Ran <ran.mariuma@gmail.com>
2015-08-29 18:12:47 +0300

    multi language support & performance improvements
    -Multi language support:
    en - English
    he - Hebrew
    de - German
    ru - Russian
    it - Italian
    fa - Persian
    
    For more please contribute or contact the developer

--- addphotosofbutton.js
+++ addphotosofbutton.js
@@ -2,39 +2,100 @@ addPhotosButton();
 
 document.addEventListener('DOMNodeInserted', addButtonIfNonExists);
 
-function addButtonIfNonExists(){
-   var photosButton = document.getElementById('photos-of-extension'); 
-   if (photosButton===null || getUserId() !== photosButton.getAttribute("target-id")){
-		addPhotosButton();
-   }
+function addButtonIfNonExists() {
+    var photosButton = document.getElementById('photos-of-extension');
+    if (photosButton === null || getUserId() !== photosButton.getAttribute("target-id")) {
+        addPhotosButton();
+    }
 }
 
-function addPhotosButton(){
-	var btn = document.createElement ("a");
-	btn.className="_6-6";
-	btn.innerText="Photos Tagged";
-	btn.setAttribute ("id","photos-of-extension");
-	var targetId =getUserId();
-	if (targetId === ""){
-		return;
-	}
-	btn.setAttribute ("target-id",targetId);
-	btn.setAttribute ("href","http://www.facebook.com/search/"+targetId+"/photos-of");
-	var parentElement = document.querySelector ("#fbTimelineHeadline ._70k div");
-	parentElement.insertBefore (btn,parentElement.children[parentElement.childNodes.length-1]);
+function addPhotosButton() {
+    var targetId = getUserId();
+    if (!targetId) {
+        return;
+    }
+    var parentElement = document.querySelector("#fbTimelineHeadline ._70k div");
+    if (parentElement) {
+        var currentLanguage = getCurrentFacebookLanguage();
+        var btn = CreateButtonElement(targetId, currentLanguage);
+        parentElement.insertBefore(btn, parentElement.children[parentElement.childNodes.length - 1]);
+    }
 }
 
-function getUserId(){
-   var pgletMainCol = document.getElementById('pagelet_timeline_main_column'); 
+/*
+ Creates a button element with all the attributes.
+ */
+function CreateButtonElement(targetId, language) {
+    var btn = document.createElement("a");
+    btn.className = "_6-6";
+    btn.innerText = getInnerTextByCurrentLanguage(language);
+    btn.setAttribute("id", "photos-of-extension");
+    btn.setAttribute("target-id", targetId);
+    btn.setAttribute("href", "http://www.facebook.com/search/" + targetId + "/photos-of");
+    return btn;
+}
 
-   if (pgletMainCol===null){
-		return "";
-   }
-   var dataGt=pgletMainCol.getAttribute("data-gt");
-   
-   var re = new RegExp("\"profile_owner\":\"([0-9]+?)\"");
-   var match = re.exec(dataGt);
+/*
+ Gets the Facebook id of the current profile page
+ */
+function getUserId() {
+    var pgletMainCol = document.getElementById('pagelet_timeline_main_column');
 
-   
+    if (pgletMainCol === null) {
+        return "";
+    }
+    var dataGt = pgletMainCol.getAttribute("data-gt");
+
+    var re = new RegExp("\"profile_owner\":\"([0-9]+?)\"");
+    var match = re.exec(dataGt);
+
+
     return match[1];
+}
+
+/*
+ Gets the inner text of the button according to the given language. Not supported language will be English
+ */
+function getInnerTextByCurrentLanguage(fbLanguage) {
+    var buttonText;
+    switch (fbLanguage) {
+        case "en":
+            buttonText = "Photos Tagged";
+            break;
+        case "he":
+            buttonText = "תמונות מתוייגות";
+            break;
+        case "de":
+            buttonText = "Mehr Fotos";
+            break;
+        case "ru":
+            buttonText = "Ещё фото";
+            break;
+        case "it":
+            buttonText = "Foto Taggati";
+            break;
+        case "fa":
+            buttonText = "عکس برچسب زده";
+            break;
+        default:
+            console.log(fbLanguage + " is not a supported language. Please contact the developers of 'Facebook photos-tagged' extension at:" +
+                "https://chrome.google.com/webstore/detail/facebook-photos-tagged/lbacdacnofcaobjedeegmoakkfbpndpp/support");
+            buttonText = "Photos Tagged";
+            break;
+    }
+    return buttonText;
+}
+
+/*
+ Gets the language of the current Facebook logged in profile
+ */
+function getCurrentFacebookLanguage() {
+    currentLanguage = "en";
+    var facebookRootElement = document.getElementById('facebook');
+
+    if (facebookRootElement === null) {
+        return currentLanguage;
+    }
+    currentLanguage = facebookRootElement.getAttribute("lang");
+    return currentLanguage;
 }
